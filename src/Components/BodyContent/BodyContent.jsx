cec80adc8386d57a332c456275926bbe6b36e857
Dilusha Lakshan <jmdilushalakshan@gmail.com>
2026-05-17 16:51:20 +0530

    imported CSS files into their JSX files

--- src/Components/BodyContent/BodyContent.jsx
+++ src/Components/BodyContent/BodyContent.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './BodyContent.css'
 
 function BodyContent() {
   return (
--- src/Components/HeaderContent/HeaderContent.jsx
+++ src/Components/HeaderContent/HeaderContent.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './HeaderContent.css'
 
 function HeaderContent() {
   return (
